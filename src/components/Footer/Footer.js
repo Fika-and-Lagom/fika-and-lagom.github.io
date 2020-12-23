21b9fd8c6863738fa15ea3b1da952d19c4b79592
Sarah Huffman <sarahericson33@gmail.com>
2020-12-23 13:58:47 -0600

    Adding MaterialUI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./Footer.css";
 import { FiTwitter } from 'react-icons/fi'
 import { FiInstagram } from 'react-icons/fi'
+import { Button } from '@material-ui/core';
 
-function Footer({}) {
+function Footer() {
     return (
         <div className="footer">
+          <Button color="primary">Hello World</Button>
           <FiTwitter/>
           <FiInstagram/>
 
