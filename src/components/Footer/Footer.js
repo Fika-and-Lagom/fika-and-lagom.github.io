f6793e16bb79ff6c01cf2ac48991f00731e62dee
Sarah Huffman <sarahericson33@gmail.com>
2020-12-23 14:19:19 -0600

    Update build settings

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./Footer.css";
 import { FiTwitter } from 'react-icons/fi'
 import { FiInstagram } from 'react-icons/fi'
-import { Button } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+
 
 function Footer() {
     return (
         <div className="footer">
-          <Button color="primary">Hello World</Button>
-          <FiTwitter/>
-          <FiInstagram/>
-
+        <Container>
+          <Row>
+            <IconButton size="large" color="secondary"><FiTwitter/></IconButton>
+            <IconButton size="large" color="secondary"><FiInstagram/></IconButton>
+          </Row>
+        </Container>
         </div>
     )
 }
